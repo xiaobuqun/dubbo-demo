e9dd5adeff24aaf81655cf92265d60170163e486
tom_glb <goodMorning_glb@163.com>
2020-03-31 19:40:48 +0800

    新增test

---
+++ src/main/java/test/demo/test.java
@@ -0,0 +1,14 @@
+package test.demo;
+
+/**
+ * @Title: test
+ * @Description:
+ * @Auther:
+ * @Version: 1.0
+ * @create 2020/3/31 19:39
+ */
+public class test {
+    void test(){
+        System.out.println(111);
+    }
+}
